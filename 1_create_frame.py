dc97ed3b0e38a10d79bfd2287a611750b2c49948
seopmin <89916652+seopmin@users.noreply.github.com>
2022-05-26 07:44:33 +0900

    Update 1_create_frame.py

--- 1_create_frame.py
+++ 1_create_frame.py
@@ -13,6 +13,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 # 화면 타이틀 설정
 pygame.display.set_caption("Nado Game")  # 게임 이름
 
+
 # 이벤트 루프
 running = True  # 게임이 진행중인가?
 while running:

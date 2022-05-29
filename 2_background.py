6d16dd6b0cfb1caf41ac10fc439cdb2baedc7380
seopmin <89916652+seopmin@users.noreply.github.com>
2022-05-29 15:44:10 +0900

    Update 2_background.py

--- 2_background.py
+++ 2_background.py
@@ -7,6 +7,7 @@ screen_width = 480  # 가로 크기
 screen_height = 640 # 세로 크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+
 # 화면 타이틀 설정
 pygame.display.set_caption("Nado Game")  # 게임 이름
 
